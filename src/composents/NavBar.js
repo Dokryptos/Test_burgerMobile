cdcc9a13a56be4b49f1d87f99b9028bfb1d756c5
Dok <pierredok@outlook.fr>
2023-07-22 18:21:50 +0200

    Add Design for have mobile version more complexe

---
+++ src/composents/NavBar.js
@@ -0,0 +1,31 @@
+import React,{ useState } from 'react'
+import classNames from 'classnames'
+import './Navbar.css'
+
+function NavBar(){
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu)
+    };
+
+    return(
+        <nav>
+            <div className='logo'>Mon logo</div>
+            <div className={classNames ('navbar', {'open': openMenu})}>
+                <ul>
+                    <li><a href='#Accueil'>Accueil</a></li>
+                    <li><a href='#services'>Service</a></li>
+                    <li><a href='#aboutUs'>About Us</a></li>
+                    <li><a href='#contact'>Contact</a></li>
+                </ul>
+            </div>
+            <div className='burger' onClick={toggleMenu}></div>
+            <button className='burger' onClick={toggleMenu}>
+                <span className='burger-bar'></span>
+            </button>
+        </nav>
+    )
+}
+
+export default NavBar
